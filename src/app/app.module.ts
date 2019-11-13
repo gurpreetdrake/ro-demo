2a4d5f1d1ea4f4111b72082aa8e34382fb10c7bd
VivekDrake <ms.drake@outlook.com>
2019-11-13 16:07:05 +0530

    merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {ApiServiceService} from './api.service'
+import {ApiServiceService} from './Service/api.service'
 import { WarrantyreportComponent } from './warrantyreport/warrantyreport.component';
 import { HomeComponent } from './home/home.component';
 
