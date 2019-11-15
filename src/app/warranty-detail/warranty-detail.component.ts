5a04a10c8d87f1c20dd7cd6a7907ffc80358d45e
VivekDrake <ms.drake@outlook.com>
2019-11-15 19:30:15 +0530

    added provision to print ui

--- src/app/warranty-detail/warranty-detail.component.ts
+++ src/app/warranty-detail/warranty-detail.component.ts
@@ -18,5 +18,12 @@ export class WarrantyDetailComponent implements OnInit {
         this.item = params;
       });
   }
-
+  onPrint() {
+    const printContent = document.getElementById("report");
+    const WindowPrt = window.open('', '', 'left=0,top=15,width=700,height=700,toolbar=0,scrollbars=0,status=0');
+    WindowPrt.document.write(printContent.innerHTML);
+    WindowPrt.document.close();
+    WindowPrt.focus();
+    WindowPrt.print();
+    }
 }
